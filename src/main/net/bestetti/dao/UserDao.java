881e8bf7b168463a689430862f9fcb88479e777f
Sergio Bestetti <sbestetti@gmail.com>
2016-09-02 16:45:08 +0100

    .

--- src/main/net/bestetti/dao/UserDao.java
+++ src/main/net/bestetti/dao/UserDao.java
@@ -1,11 +1,9 @@
 package main.net.bestetti.dao;
 
 import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import main.net.bestetti.model.User;
 
@@ -16,17 +14,22 @@ public class UserDao {
 	private EntityManager em;
 	
 	public int add(User user) {
-		try {
+		if (this.getUserByEmail(user).isEmpty()) {
 			em.persist(user);
 			return 0;
-		} catch (PersistenceException e) {
-			System.out.println("Error adding user! " + user.getEmail() + " already exists.");
-			return 1;
 		}
+		return 1;		
 	}
 	
 	public List<User> getUsersOnly() {
 		TypedQuery<User> query = em.createQuery("SELECT u FROM User u", User.class);
+		return query.getResultList();
+	}
+	
+	public List<User> getUserByEmail(User user) {
+		String jpql = "SELECT u FROM User u WHERE u.email = :email";
+		TypedQuery<User> query = em.createQuery(jpql, User.class);
+		query.setParameter("email", user.getEmail());
 		return query.getResultList();
 	}
 
