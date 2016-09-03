9dacac4fad91ab82ab0f542f1c67220c133c22ae
Sergio Bestetti <sbestetti@gmail.com>
2016-09-03 19:20:01 +0100

    .

--- src/main/net/bestetti/mb/LoginBean.java
+++ src/main/net/bestetti/mb/LoginBean.java
@@ -1,22 +1,29 @@
 package main.net.bestetti.mb;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import main.net.bestetti.dao.UserDao;
+import main.net.bestetti.model.User;
 
+@SessionScoped
 @ManagedBean
-@ViewScoped
-public class LoginBean {
+public class LoginBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private UserDao dao;
 	private String email;
 	private String password;
 	private boolean showErrorMessages = false;
+	private User loggedUser = new User();
 	
 	public String login() {
 		if (dao.checkLogin(email, password)) {
+			loggedUser = dao.getUserByEmail(email).get(0);
+			System.out.println("Email on loggedUser from LoginBean: " + loggedUser.getEmail());
 			return "/menu.xhtml?faces-redirect=true";
 		}
 		showErrorMessages = true;
@@ -46,7 +53,13 @@ public class LoginBean {
 	public void setShowErrorMessages(boolean showErrorMessages) {
 		this.showErrorMessages = showErrorMessages;
 	}
-	
-	
+
+	public User getLoggedUser() {
+		return loggedUser;
+	}
+
+	public void setLoggedUser(User loggedUser) {
+		this.loggedUser = loggedUser;
+	}
 
 }
---
+++ src/main/net/bestetti/util/JSFPhaseListener.java
@@ -0,0 +1,26 @@
+package main.net.bestetti.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class JSFPhaseListener implements PhaseListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void afterPhase(PhaseEvent arg0) {
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return null;
+	}
+	
+}
