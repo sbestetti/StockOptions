434ec703b78add3fa8031323fddc8d691e6f1291
Sergio Bestetti <sbestetti@gmail.com>
2016-09-07 01:32:48 +0100

    .

--- src/main/net/bestetti/dao/OperationDao.java
+++ src/main/net/bestetti/dao/OperationDao.java
@@ -3,34 +3,35 @@ package main.net.bestetti.dao;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
-
+import main.net.bestetti.mb.LoginBean;
 import main.net.bestetti.model.Operation;
 import main.net.bestetti.model.OperationCost;
 import main.net.bestetti.model.User;
 
-@Named
+@Named @RequestScoped
 public class OperationDao implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@PersistenceContext
+	@PersistenceContext(unitName = "StockOptions")
 	private EntityManager em;
 	
-	public OperationDao() {
-		System.out.println("OperationDao created");
-	}
+	@Inject
+	private LoginBean loginBean;
 	
 	@Transactional
 	public void add (Operation op, OperationCost oc) {
 		op.setCreated(new Date());
 		oc.setOperation(op);
 		op.setOc(oc);
+		op.setUser(loginBean.getUser());
 		em.persist(op);
 		em.persist(oc);
 	}
